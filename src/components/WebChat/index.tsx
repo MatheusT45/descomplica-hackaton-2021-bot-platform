a28f610370818e437be13212eb6f7df22a00d465
MatheusT45 <matheus.tavares45@gmail.com>
2021-08-05 20:24:26 -0300

    add border radius for bubbles

--- src/components/WebChat/index.tsx
+++ src/components/WebChat/index.tsx
@@ -6,7 +6,9 @@ export default function WebChat() {
   const styleSet: any = createStyleSet({
     rootHeight: '40%',
     rootWidth: '30%',
-    backgroundColor: 'mediumseagreen'
+    backgroundColor: 'mediumseagreen',
+    bubbleBorderRadius: 8,
+    bubbleFromUserBorderRadius: 8,
  });
   const directLine = useMemo(() => createDirectLine({ token: process.env.REACT_APP_WEBCHAT_TOKEN }), []);
 
