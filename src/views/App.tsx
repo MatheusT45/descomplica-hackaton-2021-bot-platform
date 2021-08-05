6d03e443c57cc5234aeccb9f75c2bac8f1698c7e
MatheusT45 <matheus.tavares45@gmail.com>
2021-08-05 14:08:55 -0300

    remove create-react-app default stuff

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -1,21 +1,16 @@
-import logo from '../logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://descomplica.com.br/guia-de-carreiras/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Olá!
         </a>
       </header>
     </div>
