fc1582ae3155ead7c7622596d687bcdf91a3e391
MatheusT45 <matheus.tavares45@gmail.com>
2021-08-05 20:24:55 -0300

    refactor fab button style

--- src/components/FloatingActionButton/index.tsx
+++ src/components/FloatingActionButton/index.tsx
@@ -3,7 +3,7 @@ import './index.css';
 export default function FloatingActionButton(props: any) {;
   return (
     <div className="fab">
-      <button className="main" onClick={props.onClick}>
+      <button onClick={props.onClick}>
         👋
       </button>
     </div>
