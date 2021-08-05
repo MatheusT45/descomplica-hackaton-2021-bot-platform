d5a0a47e2451520de34ad96282014774046b2e80
MatheusT45 <matheus.tavares45@gmail.com>
2021-08-05 14:58:35 -0300

    green background on chat

--- src/components/WebChat.tsx
+++ src/components/WebChat.tsx
@@ -6,7 +6,7 @@ export default function WebChat() {
   const styleSet: any = createStyleSet({
     rootHeight: '80%',
     rootWidth: '80%',
-    backgroundColor: 'paleturquoise'
+    backgroundColor: 'mediumseagreen'
  });
   const directLine = useMemo(() => createDirectLine({ token: 'F1mceHZdYRs.zMTThcDoFRJ6WzQ97xyL_R2u9ObTfKGymdaotQYcDgA' }), []);
 
