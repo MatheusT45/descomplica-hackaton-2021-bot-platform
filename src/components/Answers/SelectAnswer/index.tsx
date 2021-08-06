8ec9f36759c3c7bc8583160b6fb004c2da2d975a
MatheusT45 <matheus.tavares45@gmail.com>
2021-08-06 16:47:55 -0300

    add images to selector

--- src/components/Answers/SelectAnswer/index.tsx
+++ src/components/Answers/SelectAnswer/index.tsx
@@ -17,12 +17,32 @@ function SelectAnswer() {
   return (
     <div className="persona-select">
       <div className="persona-row">
-        <span className="persona" onClick={() => handleAnswer(answers[count].options[0])}>{answers[count].options[0]}</span>
-        <span className="persona" onClick={() => handleAnswer(answers[count].options[1])}>{answers[count].options[1]}</span>
+        <span 
+          className="persona"
+          onClick={() => handleAnswer(answers[count].options[0])}>
+            { answers[count].images.length > 0 ? <img alt={answers[count].labels[0]} src={answers[count].images[0]}/> : answers[count].labels[0]}
+        </span>
+        <span
+          className="persona"
+          onClick={() => handleAnswer(answers[count].options[1])}>
+          { answers[count].images.length > 0 ? <img alt={answers[count].labels[1]} src={answers[count].images[1]}/> : answers[count].labels[1]}
+        </span>
       </div>
       <div className="persona-row">
-        {answers[count].options[2] && <span className="persona"onClick={() => handleAnswer(answers[count].options[2])}>{answers[count].options[2]}</span>}
-        {answers[count].options[3] && <span className="persona"onClick={() => handleAnswer(answers[count].options[3])}>{answers[count].options[3]}</span>}
+        {answers[count].options[2] && 
+          <span
+            className="persona"
+            onClick={() => handleAnswer(answers[count].options[2])}>
+            { answers[count].images.length > 0 ? <img alt={answers[count].labels[2]} src={answers[count].images[2]}/> : answers[count].labels[2]}
+          </span>
+        }
+        {answers[count].options[3] &&
+          <span 
+            className="persona"
+            onClick={() => handleAnswer(answers[count].options[3])}>
+            { answers[count].images.length > 0 ? <img alt={answers[count].labels[3]} src={answers[count].images[3]}/> : answers[count].labels[3]}
+          </span>
+        }
       </div>
     </div>
   );
