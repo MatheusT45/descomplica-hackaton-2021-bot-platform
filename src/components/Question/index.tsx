67290b0c681d02769e74946bb54ccfea3ec2240c
MatheusT45 <matheus.tavares45@gmail.com>
2021-08-06 10:09:49 -0300

    add empty subtitle

--- src/components/Question/index.tsx
+++ src/components/Question/index.tsx
@@ -2,11 +2,10 @@ import YesNoAnswer from '../Answers/YesNoAnswer';
 import './index.css';
 
 function Question() {
-
   return (
     <div>
       <h1 className="question-title">E aí, Pronto pra começar?</h1>
-      <h3 className="question-subTitle"></h3>
+      <h3 className="question-subTitle"> </h3>
       <div className="answer-block">
         <YesNoAnswer yesAnswer="Sim!" noAnswer="Sim vermelho"/>
       </div>
