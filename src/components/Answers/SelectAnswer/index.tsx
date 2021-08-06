069aabf16c28cfa2bc897c9032a84fd3314c06e4
MatheusT45 <matheus.tavares45@gmail.com>
2021-08-06 18:49:24 -0300

    remove background when it is image

--- src/components/Answers/SelectAnswer/index.tsx
+++ src/components/Answers/SelectAnswer/index.tsx
@@ -14,33 +14,35 @@ function SelectAnswer() {
     }
   }
 
+  const hasImage = () => answers[count].images.length > 0;
+
   return (
-    <div className="persona-select">
-      <div className="persona-row">
+    <div className="select">
+      <div className="row">
         <span 
-          className="persona"
+          className={`option ${hasImage() ? '' : 'without-image'}`}
           onClick={() => handleAnswer(answers[count].options[0])}>
-            { answers[count].images.length > 0 ? <img alt={answers[count].labels[0]} src={answers[count].images[0]}/> : answers[count].labels[0]}
+            { hasImage() ? <img alt={answers[count].labels[0]} src={answers[count].images[0]}/> : answers[count].labels[0]}
         </span>
         <span
-          className="persona"
+          className={`option ${hasImage() ? '' : 'without-image'}`}
           onClick={() => handleAnswer(answers[count].options[1])}>
-          { answers[count].images.length > 0 ? <img alt={answers[count].labels[1]} src={answers[count].images[1]}/> : answers[count].labels[1]}
+          { hasImage() ? <img alt={answers[count].labels[1]} src={answers[count].images[1]}/> : answers[count].labels[1]}
         </span>
       </div>
-      <div className="persona-row">
+      <div className="row">
         {answers[count].options[2] && 
           <span
-            className="persona"
+            className={`option ${hasImage() ? '' : 'without-image'}`}
             onClick={() => handleAnswer(answers[count].options[2])}>
-            { answers[count].images.length > 0 ? <img alt={answers[count].labels[2]} src={answers[count].images[2]}/> : answers[count].labels[2]}
+            { hasImage() ? <img alt={answers[count].labels[2]} src={answers[count].images[2]}/> : answers[count].labels[2]}
           </span>
         }
         {answers[count].options[3] &&
           <span 
-            className="persona"
+            className={`option ${hasImage() ? '' : 'without-image'}`}
             onClick={() => handleAnswer(answers[count].options[3])}>
-            { answers[count].images.length > 0 ? <img alt={answers[count].labels[3]} src={answers[count].images[3]}/> : answers[count].labels[3]}
+            { hasImage() ? <img alt={answers[count].labels[3]} src={answers[count].images[3]}/> : answers[count].labels[3]}
           </span>
         }
       </div>
